7a3193d045d4457207d206351dce4794c8eac581
Kuiyuan Shao <keiran.shao@Gmail.com>
2022-08-08 17:59:03 +1200

    Add files via upload

--- dataSim.R
+++ dataSim.R
@@ -1,42 +1,37 @@
 ##############################################################################################################################
 ######################################################## Simulation ##########################################################
 ##############################################################################################################################
-library(mvtnorm)
-set.seed(2022)
+library(purrr)
+library(optimall)
 
-n <- 50000
-y1 <- as.factor(rbinom(n, size = 1, prob = c(0.4, 0.6)))
-
-#Construct correlation
-ind <- y1 == 0
-y2 <- rep(0, n)
-y2[ind] <- rpois(sum(ind), lambda = 50)
-y2[!ind] <- rpois(sum(!ind), lambda = 40)
-
-#Consturct a variance-covariance matrix for the 10 X covariates.
-cov_m <- matrix(seq(5, 1, length.out = 100), 10)
-cov_m[lower.tri(cov_m)] = t(cov_m)[lower.tri(cov_m)]
-x_s <- rmvnorm(n, mean = rep(10, 10) + seq(10, 1), sigma = cov_m)
-x_s[, 1] <- 0.2*y2
-full_data <- data.frame(y1, y2, x1 = x_s[, 1], x2 = x_s[, 2], x3 = x_s[, 3],
-                        x4 = x_s[, 4], x5 = x_s[, 5], x6 = x_s[, 6], x7 = x_s[, 7],
-                        x8 = x_s[, 8], x9 = x_s[, 9], x10 = x_s[, 10])
-
-rm(y1, y2, x_s)
-
-############### Validated Data #############
-R_ind <- sample(1:n, n/10)
-validated <- full_data[R_ind, ]
-############### Unvalidated Data ###########
-unvalidated <- full_data[-R_ind, ]
-#Adding measurement errors
-n2 <- dim(unvalidated)[1]
-unvalidated$y1[sample(1:n2, n2/10)] <- 
-  as.factor(rbinom(n2/10, size = 1, prob = c(0.5, 0.5)))
-unvalidated$y2 <- unvalidated$y2 + rnorm(n2, 0, sd = 10)
-#Measurement errors in all 10 x variables are the same
-for (i in seq(3:dim(unvalidated)[2])){
-  unvalidated[, i] <- unvalidated[, i] + rnorm(n2, 0, sd = 5) 
+simFun <- function(N){
+  set.seed(2022)
+  Z <- rbernoulli(N, p = 0.5)
+  X <- (1 - mean(Z))/2 * rnorm(N, sd = 1)
+  error <- rnorm(N)
+  beta0 <- rnorm(N, mean = 1, sd = 1)
+  beta1 <- 0.3
+  beta2 <- 0.2
+  Y <- beta0 + beta1*X + beta2*Z
+  X_star <- X + rnorm(N, mean = 0, sd = 0.5)
+  full_data <- data.frame(Y, X, Z, X_star)
+  full_data$id <- 1:nrow(full_data)
+  return (full_data)
 }
 
-
+sampleFun <- function(full_data, n){
+  ############### Unvalidated Data #############
+  unvalidated <- full_data
+  unvalidated$X <- NA
+  ############### Phase-2 Sampling #############
+  ### Optimal Allocation
+  sampling_design <- optimum_allocation(data = unvalidated, strata = "Z", 
+                                        y = "X_star", nsample = n, method = "WrightII")
+  entire <- sample_strata(data = unvalidated, strata = "Z", id = "id", 
+                          design_data = sampling_design, design_strata = "strata",
+                          n_allocated = "stratum_size")
+  ############### Validated Data ###############
+  entire$X[entire$sample_indicator == 1] <- full_data$X[entire$sample_indicator == 1]
+  subset <- entire[entire$sample_indicator == 1, ]
+  return (list(full_data, entire, subset))
+}
